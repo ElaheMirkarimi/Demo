c46a6b4fa143966f24fbe59f2e5f858fa1f00ae0
Elahe Mirkarimi <mirkarimi.elahe@outlook.com>
2019-05-09 15:43:14 -0400

    Initial Commit.

---
+++ Demo.Infra.Data/DemoBDContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Demo.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Infra.Data
+{
+    public class DemoDBContext : DbContext
+    {
+        public DemoDBContext(DbContextOptions option) : base(option)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}

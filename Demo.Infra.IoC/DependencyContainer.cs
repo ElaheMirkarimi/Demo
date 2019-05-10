bfb661eb3f8389bb7d5ee56c92051cc0f7c19c6f
Elahe Mirkarimi <mirkarimi.elahe@outlook.com>
2019-05-10 16:46:54 -0400

    Commit Dependency Container.

---
+++ Demo.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using Demo.Application.Interfaces;
+using Demo.Application.Services;
+using Demo.Domain.Interfaces;
+using Demo.Infra.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Application Layer
+            services.AddScoped<ICourseService, CourseService>();
+
+            //Infra.Data
+            services.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
